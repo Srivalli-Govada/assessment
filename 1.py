99b6da6ce979d2ee11ac3355a4110a41ee65afad
Srivalli Govada <111562061+Srivalli-Govada@users.noreply.github.com>
2022-11-21 16:54:39 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,9 @@
-def solve(n1):
-    sign = '-' if n1<0 else ''
-    n1 = abs(n1)
-    if n1 < 3:
-        return str(n1)
-    s=''
-    while n1 != 0:
-        s = str(n1%3) + s
-        n1 = n1//3
-        return sign+s
-n1=int(input())
-print(solve(n1))
+def find(n):
+    q=n/3
+    r=n%3
+    if q==0:
+        return ''
+    else:
+        return find(int(q))+str(int(r))
+n=int(input())
+print(find(n))
