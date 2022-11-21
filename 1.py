0a51c8d98d8e62d93e8f783d4cbfe3165891d390
Srivalli Govada <111562061+Srivalli-Govada@users.noreply.github.com>
2022-11-21 15:44:59 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-
+class sol:
+  def solve(self,n):
+    s1='-' if n<0 else ''
+    n=abs(n)
+    if(n<3):
+      return str(n)
+    s=''
+    while n!=0:
+      s=str(n%3)+s
+      n=n//3
+    return s1+s
+n1=int(input)
+a=sol()
+print(a.solve(n1))
