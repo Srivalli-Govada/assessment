bc84b4c8a11cde70f886a93c0a1418a57837722c
Srivalli Govada <111562061+Srivalli-Govada@users.noreply.github.com>
2022-11-21 16:45:50 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,12 +1,8 @@
-def count(a, b, m, n):
-if ((m == 0 and n == 0) or n == 0): 
-        return 1
-    if (m == 0):
-        return 0
-    if (a[m - 1] == b[n - 1]):
-        return (count(a, b, m - 1, n - 1) + count(a, b, m - 1, n))
-    else:
-        return count(a, b, m - 1, n) 
 a = input()
 b = input()
-print(count(a, b, len(a),len(b)))
+c1=b[-1]
+c=0
+for i in a:
+    if(i==c1):
+        c+=1
+print(c)
